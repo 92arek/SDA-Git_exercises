64d74e276b8a43b9655f0910eac1c7aaa8b807a6
Arek Pamulski <a.pamulski@gmail.com>
2018-01-23 18:40:49 +0100

    Calculator

--- src/main/java/com/company/gitExercises/Calculator.java
+++ src/main/java/com/company/gitExercises/Calculator.java
@@ -1,4 +1,16 @@
-package PACKAGE_NAME;
+package com.company.gitExercises;
 
 public class Calculator {
+    public float plus(float a, float b) {
+        return a + b;
+    }
+    public float minus(float a, float b) {
+        return a - b;
+    }
+    public float times(float a, float b) {
+        return a * b;
+    }
+    public float by(float a, float b) {
+        return a / b;
+    }
 }
